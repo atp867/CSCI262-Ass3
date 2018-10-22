3ed59a1b8bc0c59f328cc356109cd4a6227f7440
of767 <44015121+of767@users.noreply.github.com>
2018-10-22 21:23:38 +1100

    Update alert.cpp

--- alert.cpp
+++ alert.cpp
@@ -9,6 +9,7 @@
 *************************************/
 #include "alert.h"
 #include <iomanip>
+#include <iostream>
 
 using namespace std;
 
@@ -188,42 +189,186 @@ void alertEngine::readUserFile(activityEngine& activity)
 }
 
 // Caluclates the anomaly thingos idk
-void alertEngine::calcAnomalyCount(activityEngine activity)
+void alertEngine::readCurrDays(int i)
 {
+	int type;
+	
+	day d;
+		
+		stringstream ss;
+		ss << i;
+		string file = "day" + ss.str() + ".txt";
+		
+		ifstream fin;
+		fin.open(file);
+		if(!fin.good())
+        {
+            std::cerr << "File does not exist" << std::endl;
+            exit(5);
+        }
+        
+        string temp = ""; //stores vehicle type for calculation
+        
+        while(getline(fin, temp))
+        {
+			fin.ignore(256,':'); 
+			getline(fin, temp);//Vehicle type
+	    	getline(fin, temp); //Vehicle Rego    
+			fin.ignore(256,':'); //Type (int)
+			fin >> type;
+			//cout << type << endl;
+	        getline(fin, temp); //Start
+	    	getline(fin, temp); //ParkingTime
+	        getline(fin, temp); //InitialSpeed
+	      	getline(fin, temp); //Location
+			getline(fin, temp); //Total
+			getline(fin, temp); //End
+			getline(fin, temp); //Blank (check for eof)
+			
+			switch(type)
+			{
+				case 0	: d.busNum++; 			break;
+				case 1	: d.motorbikeNum++; 	break;
+				case 2	: d.carNum++; 			break;
+				case 3	: d.elephantNum++; 		break;
+				case 4	: d.taxiNum++;			break;
+				case 5	: d.emergencyNum++; 	break;
+				default	: cout << "error: vehicle type switch" << endl;
+			}
+			
+			if(!temp.size())
+			{
+				break;
+			}
+		}
+		
+		/*
+		cout << endl << "READING STATS" << endl;
+		cout << "Bus: " << d.busNum << endl << "Motorbike: " << d.motorbikeNum << endl
+		<< "Red car: " << d.carNum << endl << "Elephant: " << d.elephantNum << endl
+		<< "Taxi: " << d.taxiNum << endl << "Emergency: " << d.emergencyNum << endl;
+		*/
+		
+		testDays.push_back(d);
+		
+		fin.close();
+}
+
+void alertEngine::calcAnomaly(activityEngine activity, int day)
+{
+	float volAnomCount = 0;
+	float speedAnomCount = 0;
+	float volSum;
+	float speedSum;
+	
+	/*
+	cout << endl << "ANOMALY CALC STATS" << endl;
+	cout << "Bus: " << testDays[day].busNum << endl << "Motorbike: " << testDays[day].motorbikeNum << endl
+	<< "Red car: " << testDays[day].carNum << endl << "Elephant: " << testDays[day].elephantNum << endl
+	<< "Taxi: " << testDays[day].taxiNum << endl << "Emergency: " << testDays[day].emergencyNum << endl;
+	*/
+	
+	// Volume Calcs
+	volAnomCount+=formulaCalc(testDays[day].busNum, baselineStats[day].stdDevVolume, baselineStats[day].averageVolume, 
+	vehicleStats[0].volWeight);
+	
+	volAnomCount+=formulaCalc(testDays[day].motorbikeNum, baselineStats[day].stdDevVolume, baselineStats[day].averageVolume, 
+	vehicleStats[1].volWeight);	
+	
+	volAnomCount+=formulaCalc(testDays[day].carNum, baselineStats[day].stdDevVolume, baselineStats[day].averageVolume, 
+	vehicleStats[2].volWeight);	
+	
+	volAnomCount+=formulaCalc(testDays[day].elephantNum, baselineStats[day].stdDevVolume, baselineStats[day].averageVolume, 
+	vehicleStats[3].volWeight);	
+	
+	volAnomCount+=formulaCalc(testDays[day].taxiNum, baselineStats[day].stdDevVolume, baselineStats[day].averageVolume, 
+	vehicleStats[4].volWeight);	
+	
+	volAnomCount+=formulaCalc(testDays[day].emergencyNum, baselineStats[day].stdDevVolume, baselineStats[day].averageVolume, 
+	vehicleStats[5].volWeight);	
+	
+	// Speed Calcs
+	speedAnomCount+=formulaCalc(testDays[day].busNum, baselineStats[day].stdDevVolume, baselineStats[day].averageSpeed, 
+	vehicleStats[0].speedWeight);
+	
+	speedAnomCount+=formulaCalc(testDays[day].motorbikeNum, baselineStats[day].stdDevVolume, baselineStats[day].averageSpeed, 
+	vehicleStats[1].speedWeight);	
+	
+	speedAnomCount+=formulaCalc(testDays[day].carNum, baselineStats[day].stdDevVolume, baselineStats[day].averageSpeed, 
+	vehicleStats[2].speedWeight);	
+	
+	speedAnomCount+=formulaCalc(testDays[day].elephantNum, baselineStats[day].stdDevVolume, baselineStats[day].averageSpeed, 
+	vehicleStats[3].speedWeight);	
+	
+	speedAnomCount+=formulaCalc(testDays[day].taxiNum, baselineStats[day].stdDevVolume, baselineStats[day].averageSpeed, 
+	vehicleStats[4].speedWeight);	
+	
+	speedAnomCount+=formulaCalc(testDays[day].emergencyNum, baselineStats[day].stdDevVolume, baselineStats[day].averageSpeed, 
+	vehicleStats[5].speedWeight);	
+	
 	// Header Printout
 	cout << endl << "-------------------------------------------" << endl;
 	cout << "DAY" << setw(15) << "SPEED STATUS" << setw(15) << "VOLUME STATUS" << setw(10) << "WEIGHT" << endl;
-	cout << "-------------------------------------------" << endl;
+	cout << "-------------------------------------------" << endl << endl;
 	
+	cout << volAnomCount << endl;
+	cout << speedAnomCount << endl;
 	
 }
 
+float alertEngine::formulaCalc(float val, float stdDev, float mean, int weight)
+{
+	float deduction = val - mean;
+	
+	bool negative = false;
+	if(deduction < 0)
+	{
+		negative = true;
+		deduction = fabs(deduction);
+	}
+	
+	float devDed;
+	
+	if(stdDev == 0)
+	{
+		devDed = deduction;
+	}
+	else
+	{
+		devDed = (deduction/stdDev);
+	}
+	
+	cout << val << ":" << stdDev << ":" << mean << ":" << weight << endl;
+	
+	float sum = ( devDed * weight);
+	cout << sum << endl;
+	return sum;
+}
+
 //Start Alert Engine
-void alertEngine::startEngine(activityEngine& simulation)
+void alertEngine::startEngine(activityEngine activity, analysisEngine analysis)
 {
 	bool flag = true;
 	
+	vehicleStats = activity.getVehicles();
+	
 	while(flag == true)
 	{
-		readAnalysisFile(simulation.getVehicles().size());
-		readUserFile(simulation);
+		readAnalysisFile(activity.getVehicles().size());
+		readUserFile(activity);
 		
-		cout << "Enter a number of days: ";
+		cout << "Enter number of days: ";
 		cin >> newDays;
-		
 		
 		for(int i = 0; i < newDays; i++)
 		{
-			/*
 			cout << "---------------- DAY " << i << " ----------------" << std::endl;
 			activity.genEvents();
         	activity.simDay();
         	activity.printInstances(i);
         	activity.clearInstances();
-        	*/
-        	activityEngine activity;
-        	activity.startEngine(1);
-			calcAnomalyCount(activity);
+        	readCurrDays(i);
+        	calcAnomaly(activity, i);
 		}
 		
 		/*
